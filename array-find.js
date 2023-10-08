fe64044cebd5fe7ff1f8d878e842588c1bf956c5
Mohamed Siraj <siraj@oganro.com>
2023-10-08 22:43:33 +0530

    array-find

---
+++ array-find.js
@@ -0,0 +1,11 @@
+/**
+ * find() method
+ * Array.find() method usage to find one element from array elements
+ */
+
+//example-1 - find value greater then 10
+const array1 = [5, 12, 8, 130, 44];
+
+const answer1 = array1.find((value) => value === 12);
+
+console.log(answer1);
